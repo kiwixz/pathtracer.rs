417c3703dab36168d7ffc1131ba8510cc27dbcb2
kiwixz <kiwixz@outlook.com>
2022-10-07 00:33:06 +0200

    add cornell scene

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 mod thread_pool;
+mod scene;
 
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let scene_toml = std::fs::read_to_string("scenes/cornell.toml")?;
+    let scene: scene::Scene = toml::from_str(&scene_toml)?;
+
+    println!("{}", scene.planes.len());
+
     let pool = thread_pool::Static::build(
         std::thread::available_parallelism().unwrap_or(std::num::NonZeroUsize::new(1).unwrap()),
     )?;
---
+++ src/scene.rs
@@ -0,0 +1,52 @@
+#[derive(serde::Deserialize)]
+pub struct Scene {
+    pub width: i32,
+    pub height: i32,
+    pub samples: i32,
+    pub min_bounces: i32,
+    pub max_bounces: i32,
+    pub background_color: [f64; 3],
+
+    pub camera: Camera,
+
+    pub planes: Vec<Plane>,
+    pub spheres: Vec<Sphere>,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Camera {
+    pub position: [f64; 3],
+    pub rotation: [f64; 3],
+    pub scale: [f64; 3],
+    pub field_of_view: f64,
+}
+
+#[derive(serde::Deserialize)]
+pub enum Reflection {
+    #[serde(rename = "diffuse")]
+    Diffuse,
+
+    #[serde(rename = "refractive")]
+    Refractive,
+
+    #[serde(rename = "specular")]
+    Specular,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Plane {
+    pub position: [f64; 3],
+    pub rotation: [f64; 3],
+    pub reflection: Reflection,
+    pub emission: [f64; 3],
+    pub color: [f64; 3],
+}
+
+#[derive(serde::Deserialize)]
+pub struct Sphere {
+    pub position: [f64; 3],
+    pub radius: f64,
+    pub reflection: Reflection,
+    pub emission: [f64; 3],
+    pub color: [f64; 3],
+}
